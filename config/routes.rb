951b7f7fd4d1055bd8c5c8646d1c1368f38f529f
Mariusz <dammariusz@o2.pl>
2018-08-21 00:36:43 +0100

    config

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,7 @@
 Rails.application.routes.draw do
   get 'welcome/index'
 
+  resources :contact_me
   resources :contacts, only: [:new, :create]
   root to:"welcome#index"
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
