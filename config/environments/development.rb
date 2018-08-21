a0c9de9048ca444223d520e811bf8819301e90dc
Mariusz <dammariusz@o2.pl>
2018-08-21 01:30:42 +0100

    config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -55,8 +55,8 @@ Rails.application.configure do
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
 
-  config.action_mailer.default_url_options = { host: 'http://mariuszdam.herokuapp.com/}''
-  config.action_mailer.delivery_method = smtp
+  config.action_mailer.default_url_options = { host: 'http://mariuszdam.herokuapp.com}''
+  config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
     :address              => 'smtp.gmail.com',
