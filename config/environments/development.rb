a7cd4a5c559c5b95ebd60540fbbddf47b3502346
Mariusz <dammariusz@o2.pl>
2018-08-21 01:33:47 +0100

    config

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -54,21 +54,6 @@ Rails.application.configure do
 
   # Raises error for missing translations
   # config.action_view.raise_on_missing_translations = true
-
-  config.action_mailer.default_url_options = { host: 'http://mariuszdam.herokuapp.com}''
-  config.action_mailer.delivery_method = :smtp
-
-  ActionMailer::Base.smtp_settings = {
-    :address              => 'smtp.gmail.com',
-    :port                 => '587',
-    :authentication       => :plain,
-    :user_name            => ENV['truesajgon@gmail.com'],
-    :password             => ENV['jeremiasz1'],
-    :domain               => 'gmail.com',
-    :enable_starttls_auto => true
-    }
-
-
   # Use an evented file watcher to asynchronously detect changes in source code,
   # routes, locales, etc. This feature depends on the listen gem.
   # config.file_watcher = ActiveSupport::EventedFileUpdateChecker
--- config/environments/production.rb
+++ config/environments/production.rb
@@ -91,4 +91,17 @@ Rails.application.configure do
 
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
+
+  config.action_mailer.default_url_options = { host: 'http://mariuszdam.herokuapp.com}''
+  config.action_mailer.delivery_method = :smtp
+
+  ActionMailer::Base.smtp_settings = {
+    :address              => 'smtp.gmail.com',
+    :port                 => '587',
+    :authentication       => :plain,
+    :user_name            => ENV['truesajgon@gmail.com'],
+    :password             => ENV['jeremiasz1'],
+    :domain               => 'gmail.com',
+    :enable_starttls_auto => true
+    }
 end
