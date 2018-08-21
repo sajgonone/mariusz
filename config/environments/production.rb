0669eb7c309d2163a436b52acb15d91c3290540e
Mariusz <dammariusz@o2.pl>
2018-08-21 01:40:08 +0100

    config

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -92,16 +92,16 @@ Rails.application.configure do
   # Do not dump schema after migrations.
   config.active_record.dump_schema_after_migration = false
 
-  config.action_mailer.default_url_options = { host: 'http://mariuszdam.herokuapp.com}''
+  config.action_mailer.default_url_options = { host: 'http://mariuszdam.herokuapp.com' }
   config.action_mailer.delivery_method = :smtp
 
   ActionMailer::Base.smtp_settings = {
-    :address              => 'smtp.gmail.com',
+    :address              => 'smtp.sendgrid.net',
     :port                 => '587',
     :authentication       => :plain,
-    :user_name            => ENV['truesajgon@gmail.com'],
-    :password             => ENV['jeremiasz1'],
-    :domain               => 'gmail.com',
+    :user_name            => ENV['SENDGRID_USERNAME'],
+    :password             => ENV['SENDGRID_PASSWORD'],
+    :domain               => 'heroku.com',
     :enable_starttls_auto => true
     }
 end
